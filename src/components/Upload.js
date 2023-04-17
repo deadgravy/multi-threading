10c0f90ffc4524e8f4e41afa0a9b364c9e39b46a
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-04-17 19:39:22 +0800

    changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import imageCompression from 'browser-image-compression';
 import LazyImage from './LazyImage';
 
 function ImageUploader() {
@@ -17,41 +16,49 @@ function ImageUploader() {
     setImages(files);
   };
 
-  const compressImages = async (images) => {
-    console.log("Input images:", images); // Add this line
-    const compressedImages = await Promise.all(
-      images.map((image) =>
-        imageCompression(image, {
-          maxSizeMB: 1,
-          maxWidthOrHeight: 800,
-          useWebWorker: true,
-        })
-      )
-    );
-    console.log("Compressed images:", compressedImages); // Add this line
-    return compressedImages;
-  };
-
-  const handleUpload = async (endpoint) => {
+  const handleBlockingCompress = async () => {
     setUploading(true);
+    const formData = new FormData();
+    images.forEach((image) => {
+      formData.append('images', image);
+    });
     try {
-      const compressedImages = await compressImages(images);
-      const formData = new FormData();
-      compressedImages.forEach((image) => {
-        formData.append('images', image);
-      });
-
-      console.log("Form Data:", formData); // Add this line
-
-      const response = await axiosInstance.post(endpoint, formData, {
+      const response = await axiosInstance.post('/blocking-compress', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-
       setImages([]);
       setUploading(false);
+      console.log(response.data);
+      // Update the state to display the uploaded images
       const imageLocations = response.data.data.map((data) => data.Location);
+      console.log(imageLocations);
+      setUploadedImages(imageLocations);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
+
+  const handleNonBlockingCompress = async () => {
+    setUploading(true);
+    const formData = new FormData();
+    images.forEach((image) => {
+      formData.append('images', image);
+    });
+    try {
+      const response = await axiosInstance.post('/non-blocking-compress', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setImages([]);
+      setUploading(false);
+      console.log(response.data);
+      // Update the state to display the uploaded images
+      const imageLocations = response.data.data.map((data) => data.Location);
+      console.log(imageLocations);
       setUploadedImages(imageLocations);
     } catch (error) {
       console.error(error);
@@ -63,24 +70,18 @@ function ImageUploader() {
     <div>
       <div style={{ marginBottom: '20px' }}>
         <input type="file" onChange={handleFileChange} multiple />
-        <button
-          onClick={() => handleUpload('/blocking-compress')}
-          disabled={uploading || images.length === 0}
-        >
+        <button onClick={handleBlockingCompress} disabled={uploading || images.length === 0}>
           {uploading ? 'Uploading (Blocking)...' : 'Upload (Blocking)'}
         </button>
-        <button
-          style={{ marginLeft: '10px' }}
-          onClick={() => handleUpload('/non-blocking-compress')}
-          disabled={uploading || images.length === 0}
-        >
+        <button style={{marginLeft: '10px'}} onClick={handleNonBlockingCompress} disabled={uploading || images.length === 0}>
           {uploading ? 'Uploading (Non-blocking)...' : 'Upload (Non-blocking)'}
         </button>
       </div>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {uploadedImages.map((imageUrl, index) => (
           <div key={index} style={{ marginRight: '10px', marginBottom: '10px' }}>
-             <LazyImage src={imageUrl} alt={`Uploaded image ${index}`} width="200" height="200" />
+            <LazyImage src={imageUrl} alt={`Uploaded image ${index}`} width="200" height="200" />
+            {/* <img key={index} src={imageUrl} alt={`Uploaded image ${index}`} width="200" height="200" /> */}
           </div>
         ))}
       </div>
